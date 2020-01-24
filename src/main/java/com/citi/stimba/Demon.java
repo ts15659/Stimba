3c5d9ba17603bb39b4f2987c3aa9dbe5437623e2
taoshen <st15316199026@163.com>
2020-01-24 10:10:46 +0800

    add

--- src/main/java/com/citi/stimba/Demon.java
+++ src/main/java/com/citi/stimba/Demon.java
@@ -3,9 +3,8 @@ package com.citi.stimba;
 public class Demon {
 
     public void lonely(){
-        System.out.println("wostimbaaohai1");
-        System.out.println("master");
-        System.out.println("hassdsdgj");
-        System.out.println("fuck you Tao!");
+        System.out.println("wohji1");
+        System.out.println("mcter");
+        System.out.println("hgj");
     }
 }
