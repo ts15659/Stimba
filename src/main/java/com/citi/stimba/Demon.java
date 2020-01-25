85aa489b71ae2498a986502cfd01173612933832
taoshen <st15316199026@163.com>
2020-01-25 12:38:26 +0800

    add

--- src/main/java/com/citi/stimba/Demon.java
+++ src/main/java/com/citi/stimba/Demon.java
@@ -3,7 +3,7 @@ package com.citi.stimba;
 public class Demon {
 
     public void lonely(){
-        System.out.println("afgtyp");
+        System.out.println("afgcdjtyp");
         System.out.println("axiup");
         System.out.println("axopinp");
     }
