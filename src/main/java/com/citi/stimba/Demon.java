f021cbe608f6d05a3f5a49b0d93d9b851daddcb3
taoshen <st15316199026@163.com>
2020-01-26 10:17:50 +0800

    add

--- src/main/java/com/citi/stimba/Demon.java
+++ src/main/java/com/citi/stimba/Demon.java
@@ -3,7 +3,7 @@ package com.citi.stimba;
 public class Demon {
 
     public void lonely() {
-        System.out.println("bcd");
+        System.out.println("bcrtd");
         System.out.println("abc");
     }
 }
